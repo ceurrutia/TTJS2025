37037b58845e5447b9e6f915983c45bc0f18d540
Cecilia urrutia <urrutia.cici@gmail.com>
2025-04-21 17:33:39 -0300

    add badges, css for nav and media querys

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 
-    // today
+    // Dia de hoy
     
     function showDay() {
         const now = new Date();
         const options = { year: 'numeric', month: 'long', day: 'numeric' }; 
-        const feformat = now.toLocaleDateString('en-En', options); 
-        document.getElementById('fecha-actual').textContent = feformat;
+        const forformat = now.toLocaleDateString('en-En', options); 
+        document.getElementById('fecha-actual').textContent = forformat;
     }
 
     showDay(); 
+
+    // Toogle hamburguesa para moviles
+
+    const toggle = document.getElementById('navToggle');
+    const menu = document.getElementById('navMenu');
+
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
 
